7f4bda9c8e91e74b57c8c2db612faa4847e2980c
Joe Betz <jbetz@linkedin.com>
2014-03-17 10:16:27 -0700

    add licensing

--- frontend/app/com/linkedin/restsearch/search/RestSearchAnalyzer.scala
+++ frontend/app/com/linkedin/restsearch/search/RestSearchAnalyzer.scala
@@ -1,3 +1,19 @@
+/*
+   Copyright (c) 2014 LinkedIn Corp.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package com.linkedin.restsearch.search
 
 import java.io.Reader
@@ -9,7 +25,7 @@ import org.apache.lucene.util.Version
 
 /**
  * Lucene analyzer used to normalize the search query string.
- * 
+ *
  * @author jbetz
  *
  */
